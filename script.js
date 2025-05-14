eb172349efb6c362d25d6cd508ca83cddef0a46c
swiftybo <leungjordan28@gmail.com>
2025-05-14 17:54:17 +0100

    Added in js code to create grid divs and entire game grid.
    Added in js code to create the resize button.
    
    Added in js code to be able to reisze the grid sqaures according to the number of squares in the grid.

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 "use strict";
 
 const gridContainer = document.querySelector(".grid-container");
+const gameSection = document.querySelector(".game-section");
+const gridDiv = document.querySelectorAll(".grid-div");
+const resizeButton = document.querySelector(".button");
 
-// Creating the grid div:
-const gridDiv = document.createElement("div");
-gridDiv.classList.add("grid-div");
-gridDiv.textContent = "New div!";
-gridContainer.appendChild(gridDiv);
+const numOfSquares = 8;
+const totalSquares = numOfSquares * numOfSquares;
+let dimensions = String(getDimensions(numOfSquares)) + "px";
+
+function getDimensions(numOfSquares) {
+    return 640 / numOfSquares;
+}
+
+// Function for creating the grid div:
+function createGridDiv(dimension) {
+    // This is a local gridDiv variable (not accessible out of function, therefore there is a global variable for all gridDivs)
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add("grid-div");
+    gridDiv.classList.add(".grid-div:hover");
+    gridDiv.style.width = dimension;
+    gridDiv.style.height = dimension;
+    gridContainer.appendChild(gridDiv);
+}
+
+function createResizeButton() {
+    const resizeButton = document.createElement("button");
+    resizeButton.classList.add("button");
+    resizeButton.textContent = "Resize Grid";
+    resizeButton.style.marginBottom = "30px";
+    resizeButton.style.padding = "10px, 30px";
+    resizeButton.style.fontSize = "20px";
+    gameSection.insertBefore(resizeButton, gridContainer);
+}
+
+// Loop to create the entire game grid:
+for (let i = 0; i < totalSquares; i++) {
+    createGridDiv(dimensions);
+}
+
+createResizeButton();
