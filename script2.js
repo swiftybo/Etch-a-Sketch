cc3fc1f621c619116858a4740dfbda9ec3352a57
swiftybo <leungjordan28@gmail.com>
2025-06-09 16:42:28 +0100

    Addition of hover effect functionality so that the colour of divs change when cursor is over a div

--- script2.js
+++ script2.js
@@ -10,11 +10,12 @@ const containerGrid = document.querySelector(".grid-container");
 
 // Game Parts:
 let gridDiv;
+let gridDivAll;
 
 // Constants
 const gridContainerWidth = 640;
 
-// Functions
+// Creating html elements
 
 function createGridDiv() {
     gridDiv = document.createElement("div");
@@ -22,6 +23,18 @@ function createGridDiv() {
     gridDiv.classList.add("grid-div");
 }
 
+// Functions
+
 for (let i = 0; i < 256; i++) {
     createGridDiv();
 }
+
+function addInkEffect() {
+    gridDivAll = document.querySelectorAll(".grid-div");
+    gridDivAll.forEach(function (div) {
+        div.addEventListener("mouseenter", function () {
+            div.style.backgroundColor = "yellow";
+        });
+    });
+}
+addInkEffect();
