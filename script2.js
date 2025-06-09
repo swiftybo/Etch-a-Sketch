9301829c06c802a8c49d2dc146473a1e62ffee1e
swiftybo <leungjordan28@gmail.com>
2025-06-09 17:16:54 +0100

    Added resize button and added event listener for when button clicked.

--- script2.js
+++ script2.js
@@ -3,25 +3,44 @@
 // Elements
 
 // sections:
-const gameSection = document.querySelector(".game-section");
+const gameSection = document.querySelector(".game_section");
 
 // containers:
-const containerGrid = document.querySelector(".grid-container");
+const containerGrid = document.querySelector(".grid_container");
 
 // Game Parts:
 let gridDiv;
 let gridDivAll;
+let resizeButton;
 
 // Constants
 const gridContainerWidth = 640;
+let gridDivWidth = 640 / 16;
 
 // Creating html elements
 
 function createGridDiv() {
     gridDiv = document.createElement("div");
     containerGrid.appendChild(gridDiv);
-    gridDiv.classList.add("grid-div");
+    gridDiv.classList.add("grid_div");
 }
+
+// Creating the resize button and adding functionality
+resizeButton = document.createElement("button");
+gameSection.insertBefore(resizeButton, containerGrid);
+resizeButton.classList.add("resize_Button");
+resizeButton.textContent = "Resize Grid";
+
+resizeButton.addEventListener("click", function () {
+    const newGridSize = Math.round(
+        prompt("What size for new grid? Please enter number in range 10 - 100")
+    );
+    // Math.round coerces the prompt into a Number data type.
+    newGridSize < 10 || newGridSize > 100
+        ? alert("Can you not read instructions?")
+        : (gridDivWidth = 640 / newGridSize);
+    console.log(gridDivWidth);
+});
 
 // Functions
 
@@ -30,7 +49,7 @@ for (let i = 0; i < 256; i++) {
 }
 
 function addInkEffect() {
-    gridDivAll = document.querySelectorAll(".grid-div");
+    gridDivAll = document.querySelectorAll(".grid_div");
     gridDivAll.forEach(function (div) {
         div.addEventListener("mouseenter", function () {
             div.style.backgroundColor = "yellow";
