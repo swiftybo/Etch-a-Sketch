37c879b11aa5b2f9399bb532a8509086aaafced4
swiftybo <leungjordan28@gmail.com>
2025-06-09 16:27:35 +0100

    created constants section at top of script and initial code to generate the grid with gridDivs of width 40px to create a 16x16 grid in 640px x 640px grid container

--- script2.js
+++ script2.js
@@ -1,1 +1,27 @@
 "use strict";
+
+// Elements
+
+// sections:
+const gameSection = document.querySelector(".game-section");
+
+// containers:
+const containerGrid = document.querySelector(".grid-container");
+
+// Game Parts:
+let gridDiv;
+
+// Constants
+const gridContainerWidth = 640;
+
+// Functions
+
+function createGridDiv() {
+    gridDiv = document.createElement("div");
+    containerGrid.appendChild(gridDiv);
+    gridDiv.classList.add("grid-div");
+}
+
+for (let i = 0; i < 256; i++) {
+    createGridDiv();
+}
